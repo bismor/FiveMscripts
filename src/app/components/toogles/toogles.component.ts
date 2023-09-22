63f337a6fdfe7ccd09e8ae2c9d29230ee783ec6c
Vladislav Kosyachenko <cwat2011@yandex.ru>
2023-09-22 18:49:44 +0300

    Смена языка

--- src/app/components/toogles/toogles.component.ts
+++ src/app/components/toogles/toogles.component.ts
@@ -3,8 +3,40 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-toogles',
   templateUrl: './toogles.component.html',
-  styleUrls: ['./toogles.component.scss']
+  styleUrls: ['./toogles.component.scss'],
 })
 export class TooglesComponent {
+  engLanguage = {
+    all: 'All',
+    server: 'Server',
+    client: 'Client',
+    language: 'Language:',
+    theame: 'Color Theme',
+    dark: 'Dark',
+    white: 'White',
+    blue: 'Blue',
+    red: 'Red',
+    search: 'Search...',
+  };
+  ruLanguage = {
+    all: 'все',
+    server: 'Сервер',
+    client: 'Клиент',
+    language: 'Язык:',
+    theame: 'Цвет темы',
+    dark: 'Темная',
+    white: 'Белая',
+    blue: 'Синяя',
+    red: 'Красная',
+    search: 'Поиск...',
+  };
+  selectlang = this.ruLanguage;
 
+  switchRu(): void {
+    this.selectlang = this.ruLanguage;
+  }
+
+  switchEng(): void {
+    this.selectlang = this.engLanguage;
+  }
 }
