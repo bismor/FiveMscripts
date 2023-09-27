5705ab411809363663b0b181a6c4ad76d7a35cbf
Vladislav Kosyachenko <cwat2011@yandex.ru>
2023-09-27 18:16:23 +0300

    доп нативки и немного css

--- src/app/native-list.ts
+++ src/app/native-list.ts
@@ -48,4 +48,42 @@ export const NATIVELISTLUA: Native[] = [
     ],
     comment: 'Adds an output for the specified audio submix.',
   },
+  {
+    hash: 0xc6f43d0e,
+    apiset: 'server',
+    name: 'AddBlipForCoord',
+    arguments: [
+      {
+        namearg: 'x',
+        type: 'number',
+        description: 'The X coordinate to create the blip on.',
+      },
+      {
+        namearg: 'y',
+        type: 'number',
+        description: 'The Y coordinate.',
+      },
+      {
+        namearg: 'z',
+        type: 'number',
+        description: 'The Z coordinate.',
+      },
+    ],
+    comment:
+      'Creates a blip for the specified coordinates. You can use SET_BLIP_ natives to change the blip.',
+  },
+  {
+    hash: 0x4afd2499,
+    apiset: 'client',
+    name: 'AddMinimapOverlay',
+    arguments: [
+      {
+        namearg: 'name',
+        type: 'string',
+        description:
+          'The path to a .gfx file in the current resource. It has to be specified as a file.',
+      },
+    ],
+    comment: 'Loads a minimap overlay from a GFx file in the current resource.',
+  },
 ];
